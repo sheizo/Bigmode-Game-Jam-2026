02c773cd2c6f71a5d5fafd0a5c7f2c14d2b47e2a
aboutblank0 <josecolaco1999@gmail.com>
2026-02-07 11:45:38 +0000

    added custom button script to handle animations, added it to all buttons in the game

---
+++ SoapGame/Assets/_Scripts/UI/CustomButton.cs
@@ -0,0 +1,58 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler,
+    IPointerDownHandler,
+    IPointerUpHandler
+{
+
+    [SerializeField] private Vector3 hoverScale = Vector3.one * 1.1f;
+    [SerializeField] private Vector3 clickScale = Vector3.one * 0.9f;
+
+    [Header("Tween Settings")]
+    [SerializeField] private float tweenDuration = 0.2f;
+    [SerializeField] private Ease ease = Ease.OutBack;
+    [SerializeField] private bool ignoreTimeScale = true;
+
+
+    private Vector3 originalScale;
+    private Tween currentTween;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ScaleTo(hoverScale);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ScaleTo(originalScale);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ScaleTo(clickScale);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ScaleTo(originalScale);
+    }
+
+    private void ScaleTo(Vector3 scale)
+    {
+        currentTween?.Kill();
+
+        currentTween = transform
+            .DOScale(scale, tweenDuration)
+            .SetEase(ease)
+            .SetUpdate(ignoreTimeScale);
+    }
+}
