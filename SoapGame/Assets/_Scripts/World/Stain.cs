0c429d67f6d576dcbd931e6f9b37f48283cd7bb2
sheizo <tiagov_999@hotmail.com>
2026-02-07 00:16:12 +0000

    refactored stain logic

---
+++ SoapGame/Assets/_Scripts/World/Stain.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Stain : MonoBehaviour
+{
+    public DecalProjector stainDecal;
+    public BoxCollider stainCollider;
+}
--- SoapGame/Assets/_Scripts/World/StainSpawnPoint.cs
+++ SoapGame/Assets/_Scripts/World/StainSpawnPoint.cs
@@ -2,13 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-[System.Serializable]
-
-public class Stain
-{
-    public GameObject stainObject;
-}
-
 public class StainSpawnPoint : MonoBehaviour
 {
     [SerializeField] private List<Stain> _stainList;
@@ -17,27 +10,25 @@ public class StainSpawnPoint : MonoBehaviour
     {
         int randomStainIndex = Random.Range(0, _stainList.Count);
         Stain randomStain = _stainList[randomStainIndex];
-        
-        DecalProjector stainDecal = randomStain.stainObject.GetComponent<DecalProjector>();
-        BoxCollider stainCollider = randomStain.stainObject.GetComponent<BoxCollider>();
+
         int randomZRotation = Random.Range(0,360);
         
-        stainDecal.size = new Vector3(decalSize, decalSize, 0.5f);
-        stainCollider.size = stainDecal.size;
+        randomStain.stainDecal.size = new Vector3(decalSize, decalSize, 0.5f);
+        randomStain.stainCollider.size = randomStain.stainDecal.size;
         // stainDecal.size.z / 2 => gets us on ground level (we offset to go below so we dont hit collider)
-        stainCollider.center = new Vector3(0, 0, stainDecal.size.z / 2);
+        randomStain.stainCollider.center = new Vector3(0, 0, randomStain.stainDecal.size.z / 2);
 
-        randomStain.stainObject.transform.position = new Vector3(stainSpawnPoint.transform.position.x, 0.01f, stainSpawnPoint.transform.position.z);
-        randomStain.stainObject.transform.rotation = Quaternion.Euler(90,0,randomZRotation);
+        randomStain.transform.position = new Vector3(stainSpawnPoint.transform.position.x, 0.01f, stainSpawnPoint.transform.position.z);
+        randomStain.transform.rotation = Quaternion.Euler(90,0,randomZRotation);
         
-        randomStain.stainObject.SetActive(true);
+        randomStain.gameObject.SetActive(true);
     }
 
     public void Reset()
     {
         foreach (Stain stain in _stainList)
         {
-            stain.stainObject.SetActive(false);
+            stain.gameObject.SetActive(false);
         }
     }
 }
